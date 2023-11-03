e7b4f9c07a946560c69e570335359e4720cc739a
katbang <katr164m@stud.kea.dk>
2023-11-03 11:07:54 +0100

    menu links ændring

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,20 +1,26 @@
 import Link from "next/link";
 
-export default function Menu() {
+export default async function Menu() {
+  const res = await fetch("https://nice-dogs.vercel.app/api/dogs");
+  const pages = await res.json();
+
   return (
     <nav className=" flex gap-4">
-      <Link href="/" prefetch={false}>
-        Home
-      </Link>
-      <Link href="/dogs/henry" prefetch={false}>
-        Henry
-      </Link>
-      <Link href="/dogs/tommi" prefetch={false}>
-        Tommi
-      </Link>
-      <Link href="/dogs/pete" prefetch={false}>
-        Pete
-      </Link>
+      <ul>
+        <li>
+          <Link href="/" prefetch={false}>
+            Home
+          </Link>
+        </li>
+
+        {pages.map((page) => {
+          return (
+            <li key={page.id}>
+              <Link href={`/dogs/${page.slug}`}>{page.name}</Link>
+            </li>
+          );
+        })}
+      </ul>
     </nav>
   );
 }
